14cb4149f16ff79bcac1c7b38ec54545df65f299
Somesh 3168 <somesh3168@gmail.com>
2023-05-30 23:33:12 +0530

    Added header grid 0 resume

--- mysite/frontend/src/App.js
+++ mysite/frontend/src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <h2>React Resume</h2>
       <div className="resume">
+        <div className="card resume-header">0</div>
         <div className="card">1</div>
         <div className="card">2</div>
         <div className="card">3</div>
